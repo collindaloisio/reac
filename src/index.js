b4f348977c4130209417cea87ad03b158519ee7e
Collin D'Aloisio <collindaloisio@gmail.com>
2018-04-29 14:37:12 -0500

    code works on melted mac

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-
+  // TODO: remove the constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
 
   render() {
+    // TODO: use onClick={this.props.onClick}
+    // TODO: replace this.state.value with this.props.value
     return (
-        <button className="square" onClick={() => this.props.onClick({value:'X'})}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
@@ -24,14 +31,9 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return (
-      <Square 
-    value={this.state.squares[i]} 
-    onClick={()=>this.handleClick(i)}
-    />
-    );
+    return <Square value={this.state.squares[i]} />;
   }
-
+  
   render() {
     const status = 'Next player: X';
 
@@ -39,19 +41,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
